afed7c663cef280be763c75e84c9d370ff4a2ea6
Danna Cardenas <thedannitazz@gmail.com>
2022-01-23 21:59:51 -0500

    Se instala axios y se trae la data correspondiente desde la API

--- src/components/repositories.jsx
+++ src/components/repositories.jsx
@@ -1,11 +1,74 @@
-import React from "react";
+import React, {useEffect} from "react";
+import axios from 'axios';
 
 const user = {
-    token: "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",
+    token: "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",
     username: "dannitazz"
 }
 
+const bodyRepo = {
+  "query": `
+  query {
+    user(login: "${user.username}") {
+      repositories(first: 15, orderBy: {field: CREATED_AT, direction: DESC}) {
+        nodes {
+          createdAt
+          name
+          description
+        }
+      }
+    }
+  }
+  `
+}
+
+const bodyStar = {
+  "query": `
+  query {
+    user(login: "${user.username}") {
+     starredRepositories(first: 15, orderBy: {field:STARRED_AT, direction: DESC}) {
+       nodes{
+        name
+        description
+      }
+     }
+    }
+  }
+  `
+}
+
+const baseUrl = "https://api.github.com/graphql";
+const headers = {
+  "Content-Type": "application/json",
+  "Authorization": "bearer " + user.token
+}
+
+
+async function getRepositories() {
+  try {
+    const response = await axios({method: "post", url: baseUrl,  data: JSON.stringify(bodyRepo), headers: headers});
+    console.log(response.data.data.user);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getStarredRepositories() {
+  try {
+    const response = await axios({method: "post", url: baseUrl,  data: JSON.stringify(bodyStar), headers: headers});
+    console.log(response.data.data.user);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 function Repositories() {
+
+  useEffect(()=> { 
+    getRepositories();
+    getStarredRepositories();
+  }
+  , [])
     return (
       <>
         <p>Repositorios GitHub</p>

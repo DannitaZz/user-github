9915e3cd62c661408165d80271b32378c07f681c
Danna Cardenas <thedannitazz@gmail.com>
2022-01-23 19:59:08 -0500

    Se crea componente para listado de repositorios

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Repositories from './components/repositories';
+
+function App() {
+  return (
+    <div className="App">
+      <Repositories/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/repositories.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const user = {
+    token: "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",
+    username: "dannitazz"
+}
+
+function Repositories() {
+    return (
+      <>
+        <p>Repositorios GitHub</p>
+
+      </>
+    );
+}
+  
+export default Repositories;
